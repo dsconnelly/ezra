bfff890dd810ac64b6b736b31493220bf28ed8bf
dsconnelly <dsc254@cornell.edu>
2018-08-04 18:49:32 -0400

    working skeleton of API requests and storage

--- courses.py
+++ courses.py
@@ -2,16 +2,20 @@
 professor, and meeting times, though meeting times will probably eventually
 be refactored into another file."""
 
+import weektime as wt
+
 class Course(object):
     """Large-scale object for an entire course."""
-    def __init__(self, department, dept_short, number, title, professor):
+    def __init__(self, department, dept_short, number, title, required,
+        meeting_groups):
         """Initializes the Course object. Course attributes are set as
         read-only. The parameters are
             department : a string, like 'Earth and Atmospheric Sciences'.
             dept_short : a string, like 'EAS'.
             number : an integer, like 3420.
             title : a string, like 'Climate Dynamics'.
-            professor : a string, like 'Natalie Mahowald'."""
+            required : a set of strings like 'LEC' and 'LAB'.
+            meeting_groups : a list of MeetingGroup objects."""
         if type(department) is not str:
             raise ValueError('Department must be a string; got '
                 + str(department))
@@ -23,7 +27,7 @@ class Course(object):
         self._dept_short = dept_short
 
         if type(number) is not int:
-            raise ValueError('Course number must be a string; got '
+            raise ValueError('Course number must be an int; got '
                 + str(number))
         self._number = number
 
@@ -31,10 +35,11 @@ class Course(object):
             raise ValueError('Course title must be a string; got ' + str(title))
         self._title = title
 
-        if type(professor) is not str:
-            raise ValueError('Professor must be a string; got '
-                + str(professor))
-        self._professor = professor
+        if type(required) is not set:
+            raise ValueError('Course requirements must be a set')
+        self._required = required
+
+        self._meeting_groups = meeting_groups
 
     @property
     def department(self):
@@ -53,17 +58,70 @@ class Course(object):
         return self._title
 
     @property
-    def professor(self):
-        return self._professor
+    def required(self):
+        return self._required
+
+    @property
+    def meeting_groups(self):
+        return self._meeting_groups
 
     def __repr__(self):
         return ('Course(department=\'%s\', dept_short=\'%s\', number=%s, '
-            + 'title=\'%s\', professor=\'%s\')') % (self.department, self.dept_short,
-            self.number, self.title, self.professor)
+            + 'title=\'%s\')') % (self.department, self.dept_short,
+            self.number, self.title)
 
     def __eq__(self, other):
         return (self.department == other.department
             and self.dept_short == other.dept_short
             and self.number == other.number
-            and self.title == other.title
-            and self.professor == other.professor))
+            and self.title == other.title)
+
+class MeetingGroup(object):
+    """A MeetingGroup refers to a lecture or discussion or so on, which may
+    meet several times throughout the week."""
+    allowed_kinds = ['LEC', 'DIS', 'LAB', 'TA', 'SEM']
+    allowed_days = 'MTWRF'
+
+    def __init__(self, kind, instructor, days, start, end):
+        """Initializes the Meeting. The parameters are:
+            kind : a string like 'LEC' or 'DIS'.
+            instructor : a string, the name of the instructor.
+            days : a string like 'MWF'.
+            start : a WeekTime object holding the start time.
+            end : a WeekTime object holding the end time.
+        Though it does not, strictly speaking, matter, the start and end
+        WeekTimes should have day values of None, for good practice."""
+        if kind not in self.allowed_kinds:
+            raise ValueError('Invalid MeetingGroup type: %s' % kind)
+        if type(instructor) is not str:
+            raise ValueError('MeetingGroup professor must be a string')
+        if start >= end:
+            raise ValueError('Meeting must start before it ends')
+
+        self.kind = kind
+        self.instructor = instructor
+
+        self.meetings = []
+        for d in days:
+            try:
+                d_idx = self.allowed_days.find(d) + 1
+            except ValueError:
+                raise ValueError('Unknown day code: %s' % d)
+            m_start = wt.WeekTime(d_idx, start.hour, start.minute)
+            m_end = wt.WeekTime(d_idx, end.hour, end.minute)
+            self.meetings.append(Meeting(m_start, m_end))
+
+class Meeting(object):
+    """A Meeting refers to a single event during the week."""
+    def __init__(self, start, end):
+        """Since for now only MeetingGroup will call this, we will assume
+        that the start and end times are acceptable."""
+        self.start = start
+        self.end = end
+
+    def overlap(self, other):
+        """Determines if two Meeting objects overlap in time."""
+        # TODO: figure  out edge cases involving equality.
+        if self.start < other.start:
+            return other.start < self.end
+        return self.start < other.end
--- weektime.py
+++ weektime.py
@@ -9,6 +9,8 @@ class WeekTime(object):
     def __init__(self, day, hour, minute):
         """Initializes the WeekTime object. The parameters are:
             day : must be an integer in [1..7] inclusive. 1 is Monday.
+                Alternatively, can be None, which indicates a time independent
+                of day of the week.
             hour : must be an integer in [0..23] inclusive.
             minute : must be an integer in [0..59] inclusive."""
         self.day = day
@@ -23,10 +25,13 @@ class WeekTime(object):
 
     @day.setter
     def day(self, d):
-        if type(d) is not int:
+        if type(d) is not int and d is not None:
             raise ValueError('Day must be an integer; got ' + str(d))
-        if d < 1 or d > 7:
-            raise ValueError('Day value must be in [1..7]; got ' + str(d))
+        try:
+            if d < 1 or d > 7:
+                raise ValueError('Day value must be in [1..7]; got ' + str(d))
+        except TypeError:
+            pass
         self._day = d
 
     @property
@@ -63,3 +68,32 @@ class WeekTime(object):
         return (self.day == other.day
             and self.hour == other.hour
             and self.minute == other.minute)
+
+    def __lt__(self, other):
+        """Defines ordering on WeekTimes. Ordering is done by ordering in the
+        week, where Monday morning is the beginning and Sunday night the end."""
+        if self.day != other.day:
+            return self.day < other.day
+        if self.hour != other.hour:
+            return self.hour < other.hour
+        if self.minute != other.minute:
+            return self.minute < other.minute
+        return False
+
+    def __le__(self, other):
+        return self < other or self == other
+
+def parse_time(s):
+    """Returns a WeekTime object, given a string like '08:00AM'. Sets the
+    result to have None day."""
+    if s == '':
+        raise ValueError('Time string cannot be empty')
+    hour = int(s[:2])
+    if s[5:] == 'PM':
+        if hour != 12:
+            hour += 12
+    else:
+        if hour == 12:
+            hour = 0
+    minute = int(s[3:5])
+    return WeekTime(None, hour, minute)
