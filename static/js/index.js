d35dc5d731272c6b1bf3a88ad7e94c001787e09c
dsconnelly <dsc254@cornell.edu>
2018-08-06 10:50:46 -0400

    added requirements file

--- static/js/index.js
+++ static/js/index.js
@@ -1,24 +1,36 @@
-$( document ).ready(function () {
-    $(".classInfoCell").click(function() {
-        var dept_short = prompt("Enter the department abbreviation:");
-        var number = prompt("Enter the course number:");
+$( document ).ready(function() {
+    $(".classInfoCell").mousedown(function() {
+        if (!($(this).is(":focus"))) {
+            $(this).html("");
+        }
+    });
 
-        $.ajax({
-            url : "/search",
-            type : "POST",
-            data : JSON.stringify({
-                "dept_short" : dept_short,
-                "number" : number
-            }),
-            dataType : "json",
-            contentType: "application/json",
-            context: this,
-            success : function(data) {
-                this.innerHTML = data["dept_short"] + " " + data["number"] + " " + data["title"];
-            },
-            error : function() {
-                this.innerHTML = "class not found";
-            }
-        });
+    $(".classInfoCell").keypress(function(e) {
+        if (e.which == 13) {
+            e.preventDefault();
+            $(this).blur();
+
+            var user_query = $(this).html().split(" ");
+            var dept_short = user_query[0];
+            var number = user_query[1];
+
+            $.ajax({
+                url : "/search",
+                type : "POST",
+                data : JSON.stringify({
+                    "dept_short" : dept_short,
+                    "number" : number
+                }),
+                dataType : "json",
+                contentType: "application/json",
+                context: this,
+                success : function(data) {
+                    this.innerHTML = data["dept_short"] + " " + data["number"] + " " + data["title"];
+                },
+                error : function() {
+                    this.innerHTML = "class not found";
+                }
+            });
+        }
     });
 });
